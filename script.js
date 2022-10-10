b90a4e44147aed43dc3a2017fd24e02cb7de3f82
j37hr0 <jethro.cotton3@gmail.com>
2022-10-10 11:07:27 +0200

    started looping through array

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const mainBody = document.querySelectorAll(".main")
+// const bookBlock = document.createElement("table")
+// const bookInfo = document.createElement("p")
+// bookInfo.textContent = `Title: ${title}`
+// const tableContent = document.getElementById("currentBooks");
 
+
+let myLibrary = [{title : "test book1", author: "testauthor1", pages: 10, isRead: "yes"},
+                {title : "test book2", author: "testauthor2", pages: 20, isRead: "no"}];
 
 function createBook (title, author, nrPages, isRead) {
     this.title = title;
@@ -10,3 +18,20 @@ function createBook (title, author, nrPages, isRead) {
         return title, author, nrPages, isRead;
     }
 }
+
+function addBookToLibrary() {
+    
+}
+
+function displayBooks(myLibrary) {
+    for (let i = 0;i < 2; i++){
+        let row = tableContent.insertRow(0);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        cell1.innerHTML = myLibrary.title[i];
+    }
+}
+
+displayBooks()
